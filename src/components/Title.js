908b63ecbcb551e09d4ad0d9e3ac077e2defe628
supermay <wu_jiamei@outlook.com>
2017-07-24 21:49:12 +0200

    Set validations

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
 import './Title.css'
+import PropTypes from 'prop-types'
 
 
 // export default before class
 export default class Title extends Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired
+  }
   render(){
     return <h1 className="Title">
     {this.props.text}
