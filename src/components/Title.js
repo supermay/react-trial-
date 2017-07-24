edbe0500d8408c4d277a1dfb6d0fe0929247578e
supermay <wu_jiamei@outlook.com>
2017-07-24 22:41:19 +0200

    Add state

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,18 +5,30 @@ import PropTypes from 'prop-types'
 
 // export default before class
 export default class Title extends Component {
+  constructor(props) {
+    super ()
+    this.state = {
+      text: props.text
+    }
+  }
   static propTypes = {
     text: PropTypes.string.isRequired
   }
   renderText(){
-    return this.props.text.split(' ')
+    return this.state.text.split(' ')
     .map((word) => {
       return [word[0].toUpperCase(),word.slice(1)]
       .join('')
     }).join(' ')
   }
+
+  sayHi(){
+    this.setState({
+      text: "Are you ok?"
+    })
+  }
   render(){
-    return <h1 className="Title">
+    return <h1 className="Title" onClick = {this.sayHi.bind(this)}>
     {this.renderText()}
     </h1>
   }
