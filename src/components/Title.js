e2bb802ac7748816bdff34257963f61b90fb2e72
supermay <wu_jiamei@outlook.com>
2017-07-24 21:19:53 +0200

    Update Title class export file syntax

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
-class Title extends React.Component {
+export default class Title extends React.Component {
   render(){
     return <h1>Welcome to see Ice King</h1>
   }
 }
-export default Title;
